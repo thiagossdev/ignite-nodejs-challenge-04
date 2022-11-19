eb783095a66b075e3cccaf1d3111f2ebd848b81a
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-19 18:00:54 -0300

    Add use case create user

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,12 +1,19 @@
-import { Response, Request } from "express";
+import { Response, Request } from 'express';
 
-import { CreateUserUseCase } from "./CreateUserUseCase";
+import { CreateUserUseCase } from './CreateUserUseCase';
 
 class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { email, name } = request.body;
+
+    this.createUserUseCase.execute({
+      email,
+      name,
+    });
+
+    return response.status(201).send();
   }
 }
 
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,5 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
   name: string;
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const categoryAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (categoryAlreadyExists) {
+      throw new Error('Category already exists!');
+    }
+
+    return this.usersRepository.create({ email, name });
   }
 }
 
